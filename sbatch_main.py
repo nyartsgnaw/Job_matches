67a6def43355bc2aef9ec2f790e8aa779b631f47
nyartsgnaw <wangxj0822@gmail.com>
2018-07-21 14:32:14 -0400

    a

--- sbatch_main.py
+++ sbatch_main.py
@@ -1,10 +1,15 @@
-import os 
-
+import os
+try:
+	CWDIR = os.path.abspath(os.path.dirname(__file__))
+except:
+	CWDIR = os.getcwd()	
 import pandas as pd 
-path_exp = os.path.join(CWDIR,'./experiments/exp_logs.xlsx')
-df_exp = pd.read_excel(path_exp)
-for i in range(df_exp.shape[0]):
-    with open(os.path.join(CWDIR,'./experiments/.idx'),'w') as f:
-        idx = f.write(str(i))
-    os.system('sbatch submit_job.sh')
-    time.sleep(2)
+if __name__ == '__main__':
+    path_exp = os.path.join(CWDIR,'./experiments/exp_logs.xlsx')
+    df_exp = pd.read_excel(path_exp)
+    for i in range(df_exp.shape[0]):
+        with open(os.path.join(CWDIR,'./experiments/.idx'),'w') as f:
+            idx = f.write(str(i))
+        os.system('sbatch submit_job.sh')
+        time.sleep(1)
+        print('Successfully submit the {}th job.'.format(i))
