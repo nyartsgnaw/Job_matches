0ad2f5b1d12038aff9e66b86387172a4ce3eb475
nyartsgnaw <wangxj0822@gmail.com>
2018-08-06 12:57:45 -0400

    a

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def start_exp(exp):
 		model.compile(loss=LOSS, optimizer=adam, metrics=['mse'])
 		model = train_model(model,X_train=X_train.reshape([-1,1,TIME_STEPS,INPUT_DIM]),\
 							Y_train=Y_train,\
-							verbose=1,n_epoch=2,validation_split=0.1,patience=PATIENCE,\
+							verbose=1,n_epoch=N_EPOCH,validation_split=0.1,patience=PATIENCE,\
 							model_path=path_training_model,
 							log_path=path_training_log)
 		model.save(path_model)
