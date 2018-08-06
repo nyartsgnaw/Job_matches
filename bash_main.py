c8402606dd62f6333d7443d378a05d3865c12096
nyartsgnaw <wangxj0822@gmail.com>
2018-08-06 18:41:42 -0400

    A

---
+++ bash_main.py
@@ -0,0 +1,18 @@
+import time
+import os
+try:
+	CWDIR = os.path.abspath(os.path.dirname(__file__))
+except:
+	CWDIR = os.getcwd()	
+import pandas as pd 
+if __name__ == '__main__':
+    path_exp = os.path.join(CWDIR,'./experiments/exp_logs.xlsx')
+    df_exp = pd.read_excel(path_exp)
+    for i in range(df_exp.shape[0]):
+        exp = df_exp.iloc[i]
+        if int(exp['IS_RUN']) == 1:
+            with open(os.path.join(CWDIR,'./experiments/.idx'),'w') as f:
+                idx = f.write(str(i))
+            os.system('python main.py')
+#            time.sleep(10)
+        print('The {}th job is submitted.'.format(i))
